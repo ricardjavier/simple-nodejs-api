67d396fbfc2e221dfcf74eb6003750f152ae534e
ricardjavier <rmantilla00@gmail.com>
2024-02-09 18:23:41 -0300

    Corrección llamada de libros

--- index.js
+++ index.js
@@ -5,31 +5,32 @@ const app = express();
 
 const readData = () => {
    
-        try {
-            const data = fs.readFileSync("./db.json");
-            console.log(JSON.parse(data));
-        } catch (error) {
-            console.log(error);
-        }
-
-   }
-
-
-const writeData = (data) => {
     try {
-        fs.writeFileSync("./db.json" , JSON.stringify(data));
-} catch (error) {
-    console.log(error);
-}};
+const data = fs.readFileSync("./db.json");
+return JSON.parse(data);
+    } catch (error) {
+        console.log(error);
+};
+};
+  const writeData =(data) => {
+    try {
+    fs.writeFileSync("./db.json", JSON.stringify(data));
+        return JSON.parse(data);
+            } catch (error) {
+                console.log(error);
+        };
+  };
 
 app.get("/",(req, res) => { 
     res.send("Wellcome to my first API with nodejs!!!");
+
 });
 
 app.get("/books", (req, res) => {
-    const data  = readData();
+    const data = readData();
     res.json(data.books);
 });
+
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
